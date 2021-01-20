7c48f1b37d939169c4934d62438080219e2155fa
evgeniy volkov <volkov.e.n@gmail.com>
2021-01-20 18:31:14 +0300

    Add BotState.java

---
+++ src/main/java/ru/volkov/secondbot/api/BotState.java
@@ -0,0 +1,16 @@
+package ru.volkov.secondbot.api;
+
+public enum BotState {
+    ASK_DENSITY,
+    ASK_NAME,
+    ASK_AGE,
+    ASK_GENDER,
+    ASK_NUMBER,
+    ASK_COLOR,
+    ASK_MOVIE,
+    ASK_SONG,
+    FILLING_PROFILE,
+    PROFILE_FILLED,
+    SHOW_MAIN_MENU,
+    SHOW_HELP_MENU;
+}
