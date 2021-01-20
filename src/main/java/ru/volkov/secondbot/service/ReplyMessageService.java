73a3e958d52e0ce7f8111fb5e52c106872d97a20
evgeniy volkov <volkov.e.n@gmail.com>
2021-01-20 18:24:48 +0300

    Add ReplyMessageService

---
+++ src/main/java/ru/volkov/secondbot/service/ReplyMessageService.java
@@ -0,0 +1,24 @@
+package ru.volkov.secondbot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+@Service
+public class ReplyMessageService {
+
+    private final LocaleMessageService localeMessageService;
+
+    @Autowired
+    public ReplyMessageService(LocaleMessageService localeMessageService){
+        this.localeMessageService = localeMessageService;
+    }
+
+    public SendMessage sendReplyMessage(String chatId, String replyMessage){
+        return new SendMessage(chatId, localeMessageService.getMessage(replyMessage));
+    }
+
+    public SendMessage sendReplyMessage(String chatId, String replyMessage, Object... args){
+        return new SendMessage(chatId, localeMessageService.getMessage(replyMessage, args));
+    }
+}
